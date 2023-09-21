aa78c15671fa4e213b915fb3bea75583bdc1d3aa
Alexander Malukhin <keeonenew@yandex.ru>
2023-09-21 15:47:10 +0300

    feat: add test

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -29,4 +29,18 @@ function getUser(data) {
 
     // когда вы вызываете document.body.appendChild, содержимое DocumentFragment перемещается в DOM и из фрагмента. Фрагмент не сохраняет ссылку. Идея заключается в том, что у узла не может быть двух родителей. Это означает, что к тому времени, когда вы вызываете addEventListener, не существует никаких узлов, с которыми могли бы происходить события! Решение состоит в том, чтобы подключить прослушиватель событий перед вызовом .appendChild.
     pointTemplate.appendChild(setElement);
+
+}
+
+
+
+let tbody = document.querySelector("#data-here tbody");
+let rows = [...tbody.querySelectorAll("tr")];
+let columns = [...document.querySelectorAll("#data-here thead th")];
+for (let e of columns) e.onclick = td_click;
+
+function td_click() {
+    let i = columns.indexOf(this);
+    rows.sort((a, b) => a.cells[i].textContent.localeCompare(b.cells[i].textContent));
+    for (let e of rows) tbody.appendChild(e);
 }
