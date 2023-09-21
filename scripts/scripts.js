f8ba44a3bc21ce60d0dd756b47887db8fc0ae166
Alexander Malukhin <keeonenew@yandex.ru>
2023-09-21 16:53:23 +0300

    feat: add test innerhtml

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -19,28 +19,15 @@ async function getDataAll() {
 getDataAll()
 
 function getUser(data) {
-    const template = document.getElementById("data");
+    const setHTML = document.createElement('tbody');
+    setHTML.innerHTML = `
+        <tr>
+                <td class="one">${data.userId ?? "-"}</td>
+                <td class="two">${data.id ?? "-"}</td>
+                <td class="three">${data.title ?? "-"}</td>
+                <td class="four">${data.body ?? "-"}</td>
+            </tr>
+    `
 
-    const setElement = template.content.cloneNode(true);
-    setElement.querySelector('.one').textContent = `${data.userId ?? "-"}`;
-    setElement.querySelector('.two').textContent = `${data.id ?? "-"}`;
-    setElement.querySelector('.three').textContent = `${data.title ?? "-"}`;
-    setElement.querySelector('.four').textContent = `${data.body ?? "-"}`;
-
-    // когда вы вызываете document.body.appendChild, содержимое DocumentFragment перемещается в DOM и из фрагмента. Фрагмент не сохраняет ссылку. Идея заключается в том, что у узла не может быть двух родителей. Это означает, что к тому времени, когда вы вызываете addEventListener, не существует никаких узлов, с которыми могли бы происходить события! Решение состоит в том, чтобы подключить прослушиватель событий перед вызовом .appendChild.
-    pointTemplate.appendChild(setElement);
-
-}
-
-
-
-let tbody = document.querySelector("#data-here tbody");
-let rows = [...tbody.querySelectorAll("tr")];
-let columns = [...document.querySelectorAll("#data-here thead th")];
-for (let e of columns) e.onclick = td_click;
-
-function td_click() {
-    let i = columns.indexOf(this);
-    rows.sort((a, b) => a.cells[i].textContent.localeCompare(b.cells[i].textContent));
-    for (let e of rows) tbody.appendChild(e);
+    pointTemplate.appendChild(setHTML);
 }
